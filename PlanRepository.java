3564ade9bf62bbece6652b3b468ee81601bc82ed
hananzh399 <hananzh399@gmail.com>
2026-08-13 01:46:43 +0500

    Delete PlanRepository.java

--- PlanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.softschool.backend.repository;
-
-import com.softschool.backend.model.Plan;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlanRepository extends JpaRepository<Plan, String> {
-}
